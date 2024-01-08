25949c52cb1c62f62609764049b77d52fb912a43
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-01-08 23:34:44 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -104,82 +104,83 @@ body,
 </style>
 
 <script setup>
-useHead({
-  title: "Matta - Africa's Leading B2B Marketplace for chemicals",
-  charset: "utf-8",
-  viewport: "width=device-width, initial-scale=1",
-  meta: [
-    {
-      hid: "description",
-      name: "description",
-      content:
-        "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
-    },
-    {
-      hid: "og:title",
-      property: "og:title",
-      content: "Matta - Africa's Leading B2B Marketplace",
-    },
-    {
-      hid: "og:description",
-      property: "og:description",
-      content:
-        "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
-    },
-    {
-      hid: "og:image",
-      property: "og:image",
-      content: "https://matta.trade/img/3.png",
-    }, // Add OG image URL
-    // Twitter Tags
-    {
-      hid: "twitter:card",
-      name: "twitter:card",
-      content: "https://matta.trade/img/3.png",
-    }, // Use 'summary_large_image' for large images
-    {
-      hid: "twitter:title",
-      name: "twitter:title",
-      content: "Matta - Africa's Leading B2B Marketplace",
-    },
-    {
-      hid: "twitter:description",
-      name: "twitter:description",
-      content:
-        "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
-    },
-    {
-      hid: "twitter:image",
-      name: "twitter:image",
-      content: "URL to your Twitter image",
-    }, // Add Twitter image URL
-    {
-      name: "keywords",
-      content:
-        "Matta, Chemicals, Business, materials, manufacturers, producers, importers, raw materials, supplier",
-    }, // Add relevant keywords
-    { name: "author", content: "Success Ahon" }, // Add author information
-    { name: "robots", content: "index, follow" }, // Control search engine indexing
-    { name: "theme-color", content: "#165EF0" }, // Set the theme color for mobile browsers
-  ],
-  script: [
-    {
-      src: "https://sdk.monnify.com/plugin/monnify.js",
+useHead(
+  {
+    title: "Matta - Africa's Leading B2B Marketplace for chemicals",
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Matta - Africa's Leading B2B Marketplace",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://matta.trade/img/3.png",
+      }, // Add OG image URL
+      // Twitter Tags
+      {
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "https://matta.trade/img/3.png",
+      }, // Use 'summary_large_image' for large images
+      {
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "Matta - Africa's Leading B2B Marketplace",
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content:
+          "Matta is an online B2B platform that serves as a detailed and comprehensive market place for chemicals and materials. We aggregate demand from manufacturers, and supply from producers and importers into a single market place, creating an eco-system that ensures constant supply of essential raw materials within the economy. We also provide the necessary financial incentives and vehicles to aid the processes of supplier manufacturing, Importation and procurement.",
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "URL to your Twitter image",
+      }, // Add Twitter image URL
+      {
+        name: "keywords",
+        content:
+          "Matta, Chemicals, Business, materials, manufacturers, producers, importers, raw materials, supplier",
+      }, // Add relevant keywords
+      { name: "author", content: "Success Ahon" }, // Add author information
+      { name: "robots", content: "index, follow" }, // Control search engine indexing
+      { name: "theme-color", content: "#165EF0" }, // Set the theme color for mobile browsers
+    ],
+    script: [
+      {
+        src: "https://sdk.monnify.com/plugin/monnify.js",
 
-      defer: true,
-    },
-    {
-      src: "https://kit.fontawesome.com/c1a534ffdb.js",
-      crossorigin: "anonymous",
-      defer: true,
-    },
-    {
-      src: "https://www.googletagmanager.com/gtag/js?id=G-295L8F9LEF",
-      crossorigin: "anonymous",
-      defer: true,
-    },
-    {
-      innerHTML: `
+        defer: true,
+      },
+      {
+        src: "https://kit.fontawesome.com/c1a534ffdb.js",
+        crossorigin: "anonymous",
+        defer: true,
+      },
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-295L8F9LEF",
+        crossorigin: "anonymous",
+        defer: true,
+      },
+      {
+        innerHTML: `
             window.dataLayer = window.dataLayer || [];
             function gtag() {
               dataLayer.push(arguments);
@@ -187,12 +188,12 @@ useHead({
             gtag("js", new Date());
             gtag("config", "G-295L8F9LEF");
           `,
-      type: "text/javascript",
-      defer: true,
-      crossorigin: "anonymous",
-    },
-    {
-      innerHTML: `
+        type: "text/javascript",
+        defer: true,
+        crossorigin: "anonymous",
+      },
+      {
+        innerHTML: `
             (function (h, o, t, j, a, r) {
               h.hj = h.hj || function () {
                 (h.hj.q = h.hj.q || []).push(arguments);
@@ -205,18 +206,22 @@ useHead({
               a.appendChild(r);
             })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
           `,
-      type: "text/javascript",
-      crossorigin: "anonymous",
-      defer: true,
-    },
-  ],
-  link: [
-    {
-      rel: "stylesheet",
-      href: "https://unicons.iconscout.com/release/v4.0.0/css/line.css",
-    },
-  ],
-});
+        type: "text/javascript",
+        crossorigin: "anonymous",
+        defer: true,
+      },
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://unicons.iconscout.com/release/v4.0.0/css/line.css",
+      },
+    ],
+  },
+  {
+    mode: "client", // Load the script 'strict-dynamically' on client-side only
+  }
+);
 import { useMarketStore } from "~/stores/markets";
 import { useApplicationStore } from "~/stores/applications";
 import { getMarkets, getTechLevels } from "~/services/productservices";
