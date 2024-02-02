21df5b483acd6a3c219032d072f2f61ab8a2beaa
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-02-02 09:57:14 +0100

    update

--- components/Dashboard/layout/SideComponent.vue
+++ components/Dashboard/layout/SideComponent.vue
@@ -1,8 +1,10 @@
 <template>
-  <aside class="flex flex-col min-w-[245px] ">
-    <nav class="bg-white py-[11px] rounded-[10px] border border-[#F4F7FE] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.04)]">
+  <aside class="flex flex-col min-w-[245px]">
+    <nav
+      class="bg-white py-[11px] rounded-[10px] border border-[#F4F7FE] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.04)]"
+    >
       <ul>
-        <li v-for="item in navigation" :key="item.name">
+        <li v-for="item in mappedNav" :key="item.name">
           <NuxtLink
             :to="item.url"
             class="text-sm flex items-center px-5 border-r-[3px] border-transparent"
@@ -19,6 +21,20 @@
   </aside>
 </template>
 <script setup>
+import {
+  updateVendorInfo,
+  postStoreName,
+  getVendorInfo,
+} from "~/services/userservices";
+
+const formValues = reactive({
+  storeUrl: "",
+});
+onMounted(() => {
+  getVendorInfo().then((res) => {
+    formValues.storeSlug = res.data.data.storeName;
+  });
+});
 const authstore = useAuthStore();
 
 const navigation = [
@@ -26,78 +42,86 @@ const navigation = [
     name: "Dashboard",
     url: "/overview",
     icon: "mingcute:layout-3-line",
-    key:"overview"
+    key: "overview",
   },
   {
     name: "Profile",
     url: "/account/settings",
     icon: "lucide:user",
-    key:"account"
+    key: "account",
   },
   {
     name: "My Orders",
     url: "/procurement/my-orders",
     icon: "lucide:shopping-bag",
-    key:"my-orders"
+    key: "my-orders",
   },
   {
     name: "My Requests",
     url: "/procurement/my-requests",
     icon: "ri:hand-coin-line",
-    key:"my-requests"
+    key: "my-requests",
   },
 
   {
     name: "Shipping Addresses",
     url: "/procurement/shipping-addresses",
     icon: "ion:map-outline",
-    key:"shipping"
+    key: "shipping",
   },
 
   {
     name: "Wallet",
     url: "/wallet/home",
     icon: "ion:wallet-outline",
-    key:"wallet"
+    key: "wallet",
   },
   {
     name: "Financing requests",
     url: "/financing",
     icon: "f7:tag",
-    key:"financing"
+    key: "financing",
   },
   {
     name: "Saved items",
     url: "/account/saved-searches",
     icon: "tdesign:heart",
-    key:"saved"
+    key: "saved",
   },
   {
     name: "Storefront",
-    url: "/storefront",
+    url: "/tech-store",
     icon: "solar:shop-outline",
-    key:"storefront"
+    key: "storefront",
   },
 
   {
     name: "Company Settings",
     url: "/company/settings",
     icon: "mingcute:building-5-line",
-    key:"company"
+    key: "company",
   },
   {
     name: "User Management",
     url: "/user-management",
     icon: "lucide:users",
-    key:"user"
+    key: "user",
   },
   {
     name: "Products",
     url: "/storefront/products",
     icon: "fluent-mdl2:product-variant",
-    key:"products"
+    key: "products",
   },
 ];
+const mappedNav = computed(() => {
+  return navigation.map((i) => {
+    return {
+      ...i,
+      url: i.key === "storefront" ? `/${formValues.storeUrl}` : i.url,
+    };
+  });
+});
 const openIndex = ref([
   "Company",
   "Procurement",
